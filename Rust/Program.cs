74ad480f86d80c234501b2e5748d1f9ef5e72804
Ririto Ninigaya(Ryo Tabuki's Creator) <riritoninigaya@hotmail.com>
2024-07-08 12:26:17 +0300

    Add files via upload

---
+++ Rust/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rust
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ProcessStartInfo start = new ProcessStartInfo("cmd.exe");
+            start.WindowStyle = ProcessWindowStyle.Hidden;
+            start.CreateNoWindow = true;
+            start.Arguments = "/c start RustClient.exe";
+            Process.Start(start);
+        }
+    }
+}
